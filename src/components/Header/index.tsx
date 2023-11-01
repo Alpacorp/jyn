a6b62734bfe19b62d1589652b8f7ab6651b5f131
Alpacorp <alejandro.palacios88@gmail.com>
2023-11-01 09:46:14 -0500

    fix: edit import italic Rubik font

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -26,7 +26,7 @@ export const Header: FC = () => {
       <div>
         <img
           src={jynWhite}
-          className="h-28 w-80 m-3"
+          className="h-28 w-80 m-3 max-[420px]:w-60"
           alt="JyN Abogados Logo"
           title="JyN Abogados"
         />
